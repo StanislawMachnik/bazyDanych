f7cfb5818e1c5d08ce1981093f97f6d509c4fa81
StanislawMachnik <stanislaw.machnik@uczen.mechaniktg.pl>
2026-02-13 10:17:19 +0100

    Dodanie zależności - Android Development -> Zapisywanie danych w lokalnej...

---
+++ app/src/main/java/com/pt2/bazydanych/Wypieki.java
@@ -0,0 +1,9 @@
+package com.pt2.bazydanych;
+
+public class Wypieki {
+    private int id;
+    private String nazwa;
+    private String skladniki;
+    private int temperatura;
+    private int czas;
+}
